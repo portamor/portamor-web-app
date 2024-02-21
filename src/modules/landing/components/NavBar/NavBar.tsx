fe4b60d75e0ea6ad7707757de5f324737082608f
Victor Sandoval Valladolid <vicbol10@gmail.com>
2024-02-21 06:53:31 -0500

    feat: removed courses link

--- src/modules/landing/components/NavBar/NavBar.tsx
+++ src/modules/landing/components/NavBar/NavBar.tsx
@@ -1,51 +1,41 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  Link,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import Image from "next/image";
-import logo from "@public/logo.png";
+import { AppBar, Box, Button, Container, Link, Toolbar, Typography } from "@mui/material"
+import Image from "next/image"
+import logo from "@public/logo.png"
 
-const navItems = ["Programas", "Membresía", "Logros", "Sobre Nosotros"];
+const navItems = [
+  { href: "#", name: "Programas" },
+  { href: "#", name: "Membresía" },
+  { href: "#", name: "Logros" },
+  { href: "#", name: "Sobre Nosotros" },
+]
 
 export default function NavBar() {
   return (
-    <AppBar position='fixed' component='nav'>
+    <AppBar position="fixed" component="nav">
       <Toolbar sx={{ backgroundColor: "white" }}>
         <Container
-          maxWidth='lg'
+          maxWidth="lg"
           sx={{
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
           }}
         >
-          <Typography variant='h6' color='black' component='div'>
-            <Image src={logo} alt='logo portamor' />
+          <Typography variant="h6" color="black" component="div">
+            <Image src={logo} alt="logo portamor" />
           </Typography>
-          <Box
-            display='flex'
-            alignItems='center'
-            sx={{ fontSize: (theme) => theme.typography.body2 }}
-          >
+          <Box display="flex" alignItems="center" sx={{ fontSize: theme => theme.typography.body2 }}>
             {/* {navItems.map((item) => (
-              <Link href='#' underline='hover' color='black' key={item} mx={1}>
+              <Link href={href} underline='hover' color='black' key={item} mx={1}>
                 {item}
               </Link>
             ))} */}
-            <Link href='http://18.219.152.230' underline='hover' color='black' mx={1}>
-              Cursos
-            </Link>
           </Box>
           <Box>
-            <Button variant='contained'>Registrarse</Button>
+            <Button variant="contained">Registrarse</Button>
           </Box>
         </Container>
       </Toolbar>
     </AppBar>
-  );
+  )
 }
